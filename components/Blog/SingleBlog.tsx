4eb88dd80ac38aaea33d00d82e669ef31d94f207
The_AshadRaza <skills.keys@gmail.com>
2023-10-31 08:52:56 +0100

    danger error

--- components/Blog/SingleBlog.tsx
+++ components/Blog/SingleBlog.tsx
@@ -1,8 +1,6 @@
-
-import { Blog } from "@/types/blog";
+import  {Blog}  from "@/types/blog";
 import Image from "next/image";
 import Link from "next/link";
-
 const SingleBlog = ({ blog }: { blog: Blog }) => {
   const { title, image, paragraph, author, tags, publishDate } = blog;
   return (
