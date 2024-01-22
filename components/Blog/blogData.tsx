e3b6357756866840cce09efa56640b7a3f72cb66
Md Ashad Raza <skills.keys@gmail.com>
2024-01-22 10:41:03 -0800

    Changing the services name .

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -3,7 +3,7 @@ import { Blog } from "@/types/blog";
 const blogData: Blog[] = [
   {
     id: 1,
-    title: "New Bank Account For your Business Organization",
+    title: " INTERNAL AUDITING ",
     paragraph:
       "Navigate business finances effortlessly! Open a Business Account with us. Enjoy quick setup, digital convenience, and dedicated service. Your organization, our priority." ,
     image: "/images/blog/blog-01.png",
@@ -17,7 +17,7 @@ const blogData: Blog[] = [
   },
   {
     id: 2,
-    title: "Multi Currency Account",
+    title: " CASH MANAGEMENT ",
     paragraph:
       "Discover the freedom of a Multi-Currency Account! Manage multiple currencies effortlessly, enjoy real-time exchange rates, and conduct international transactions seamlessly. Your global finances simplified, all in one account.",
     image: "/images/blog/blog-02.jpg",
@@ -31,7 +31,7 @@ const blogData: Blog[] = [
   },
   {
     id: 3,
-    title: "Opening Digital Account",
+    title: " PAYMENT GATEWAY ",
     paragraph:
       "Instantly open a Digital Account online! Swift setup, easy transactions, and ultimate convenience at your fingertips. Your money, your control, anytime, anywhere",
     image: "/images/blog/blog-03.jpg",
@@ -45,7 +45,7 @@ const blogData: Blog[] = [
   },
   {
     id: 4,
-    title: "WPS PROCESSING FOR COMPANY",
+    title: " COMPANY PAYROLL ",
     paragraph:
       "An electronic salary transfer system that allows institutions to pay wages via banks, bureaux de change, and financial institutions approved and authorized to provide the service.",
     image: "/images/blog/blog-04.jpg",
@@ -59,7 +59,7 @@ const blogData: Blog[] = [
   },
   {
     id: 5,
-    title: "OPENING SAVING ACCOUNT FOR UAE RESIDENTS",
+    title: " MANAGEMENT ",
     paragraph:
       " In addition to documents that verify your identity, age, or address, you may also need to provide a minimum initial deposit when opening a bank account. A minimum initial deposit is an amount of money required by the bank upfront when opening a checking account, savings account, or certificate of deposit.",
     image: "/images/blog/blog-05.jpg",
