c3417dcd0312b8185452ee94e55e21b7045fe7d4
Md Ashad Raza <skills.keys@gmail.com>
2024-01-27 03:06:18 -0800

    Update Location

--- components/Contact/NewsLatterBox.tsx
+++ components/Contact/NewsLatterBox.tsx
@@ -13,16 +13,19 @@ const NewsLatterBox = () => {
       <h3 className="mb-11 border-b border-body-color border-opacity-25 pb-11 text-base font-medium leading-relaxed dark:border-white dark:border-opacity-25">
         Head Office .<br/>
       <br/>
+        <p> MPM Properties 
+        402, Al Khaleej road, Opp hayat regency, Dubai 21275 </p>
        <p>  601 RANIA TOWER 6TH FLOOR,  AL BARSHA 1 , DUBAI , UAE</p>
-      </h3>
+      </h3><hr/>
       <h3> Branches </h3>
-      <p> LEVEL 41, WEST TOWER, BAHRAIN FINANCIAL HUBOUR, KING FAISAL HIGHWAY, MANAMA BAHRAIN  </p> <br/>
-      <p> OFFICE 2/4, 1 PALL MALL EAST, LONDON SW1Y 5AU , UNITED KINGDOM </p>
+      <p> LEVEL 41, WEST TOWER, BAHRAIN FINANCIAL HUBOUR, KING FAISAL HIGHWAY, MANAMA BAHRAIN  </p>
+      <br/>
+      <hr/>
+      <p> OFFICE 2/4, 1 PALL MALL EAST, LONDON SW1Y 5AU , UNITED KINGDOM </p><hr/>
      
       <div className="location mr-4" >
 
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3612.5727296853797!2d55.2045613!3d25.116320899999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6bc01adfbe3d%3A0x433ae1ea35a7f636!2sQualityline%20Electro%20Mechanical%20LLC%D8%8C%20Rania%20Business%20Center%20-%20Office%20401%202nd%20St%20-%20Al%20Barsha%20-%20Al%20Barsha%201%20-%20Dubai!5e0!3m2!1sen!2sae!4v1705954419397!5m2!1sen!2sae" width="300" height="300" style={{border:0}}  loading="lazy" referrerPolicy="no-referrer-when-downgrade" allowFullScreen></iframe> 
-
+      <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3607.7765550394324!2d55.306480375384325!3d25.278100977658987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDE2JzQxLjIiTiA1NcKwMTgnMzIuNiJF!5e0!3m2!1sen!2s!4v1706353533040!5m2!1sen!2s" width="300" height="300" style={{border:0}}  loading="lazy" referrerPolicy="no-referrer-when-downgrade" allowFullScreen></iframe> 
 
       </div>
 
